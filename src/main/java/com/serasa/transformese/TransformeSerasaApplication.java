9d001859c94602a79140024f6acd0d4db6dd0632
Gabriel Nunes <gabrielnuunes22@gmail.com>
2023-05-18 22:53:18 -0300

    Adicionando arquivos da aula Spring

---
+++ src/main/java/com/serasa/transformese/TransformeSerasaApplication.java
@@ -0,0 +1,13 @@
+package com.serasa.transformese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransformeSerasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransformeSerasaApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into clinica (id, nome) values (1, 'Bem estar');
+insert into clinica (id, nome) values (2, 'Saúde mais');
+insert into clinica (id, nome) values (3, 'Alexander Fit');
+insert into clinica (id, nome) values (4, 'Clínica Tati');
